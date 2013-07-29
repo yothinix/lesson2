a73b099fcdaef1a1254a269d211077288dc6603b
Yothin Muangsommuk <yothin@kmi.tl>
2013-07-30 01:50:22 +0700

    test change file commit

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,3 @@
-/**
- * Created with IntelliJ IDEA.
- * User: yothi_000
- * Date: 7/20/13
- * Time: 8:50 PM
- * To change this template use File | Settings | File Templates.
- */
 public class Student {
     private String name;
 
